76adf8576c8b5a790e2a89062fee758a36c2c49f
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-11-12 10:50:40 +0400

    Create SwiftUI Project For Mediator Pattern And Relocate MediatorPattern Example With Foundation Library In MediatorPattern Folder.

---
+++ MediatorPattern/MediatorPatternUsingInSwiftUI/MediatorPatternUsingInSwiftUI/MediatorPatternUsingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediatorPatternUsingInSwiftUIApp.swift
+//  MediatorPatternUsingInSwiftUI
+//
+//  Created by Temur Chitashvili on 12.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MediatorPatternUsingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
